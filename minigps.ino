8d1803cb0e8fd7544c696f87af998adbcdda608f
Raphael Catolino <raphael.catolino@gmail.com>
2016-04-09 14:53:10 +0200

    Put the mcu to low power when idle

--- minigps.ino
+++ minigps.ino
@@ -14,9 +14,9 @@ SoftwareSerial SerialLonet(RX_A0, TX_A1); // RX = D14/A0, TX = D15/A1
 Sim808 sim808 = Sim808(SerialLonet);
 Network net = Network(sim808);
 GPS gps = GPS(sim808);
-int clock = 0;
-int fix_sent = 0;
-volatile int lo_active = 0;
+int clock = 0; // Resets each time we go to sleep and on overflow
+int last_event = 0; // Number of loop cycle since last event
+volatile int lo_active = 0; // Wether the lonet is waiting with data
 
 void lo_event() {
   lo_active = 1;
@@ -59,6 +59,7 @@ void setup() {
 
 void serialEvent() {
   serialPipe(SerialLonet);
+  last_event = 0;
 }
 
 void cmd_getstat() {
@@ -81,46 +82,58 @@ void cmd_getpos() {
   digitalWrite(LED, LOW);
 }
 
-void loop() {
-  delay(100);
-  clock += 100;
-  if (clock < 0) {
-    clock = 0;
-  }
-
-  if ((clock % 1000) == 0) {
-    if (lo_active) {
-      lo_active = 0;
-      Serial.println("Event Received from lonet");
-      String buffer;
-      buffer.reserve(MAX_SIZE);
-      delay(2*GRACE_PERIOD);
-      while (sim808.getline(buffer) != -1) {
-        Serial.println(buffer);
-        if (buffer.startsWith(F("+CMTI"))) {
-          buffer.remove(0, 7);
-          int coma = buffer.indexOf(',');
-          String mem = buffer.substring(0, coma);
-          String idx = buffer.substring(coma+1);
-          Serial.println("New SMS in " + mem + " memory. Index : " + idx);
-          String results[] = {String(), String()};
-          net.receiveSMS(idx, results);
-          Serial.println(results[0]);
-          String &cmd = results[1];
-          if (cmd == "getpos") {
-            cmd_getpos();
-          } else if (cmd == "getstat") {
-            cmd_getstat();
-          } else {
-            Serial.print("Unkown command : ");
-            Serial.println(cmd);
-          }
-        }
-        buffer.remove(0);
+void do_work() {
+  Serial.println("Event Received from lonet");
+  String buffer;
+  buffer.reserve(MAX_SIZE);
+  delay(2*GRACE_PERIOD);
+  while (sim808.getline(buffer) != -1) {
+    Serial.println(buffer);
+    if (buffer.startsWith(F("+CMTI"))) {
+      buffer.remove(0, 7);
+      int coma = buffer.indexOf(',');
+      String mem = buffer.substring(0, coma);
+      String idx = buffer.substring(coma+1);
+      Serial.println("New SMS in " + mem + " memory. Index : " + idx);
+      String results[] = {String(), String()};
+      net.receiveSMS(idx, results);
+      Serial.println(results[0]);
+      String &cmd = results[1];
+      if (cmd == "getpos") {
+        cmd_getpos();
+      } else if (cmd == "getstat") {
+        cmd_getstat();
+      } else {
+        Serial.print("Unkown command : ");
+        Serial.println(cmd);
       }
     }
+    buffer.remove(0);
   }
-  //LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); // Turn everything off until next interrupt/wdt. Draws 0.38 mA
+}
+
+void gotosleep() {
+    Serial.end();
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF); // Turn everything off until next interrupt/wdt. Draws 0.38 mA
+    Serial.begin(9600);
+}
+
+void loop() {
+  if (last_event >= 8) {
+    gotosleep();
+    clock = 0;
+  } else {
+    delay(GRACE_PERIOD);
+    clock += GRACE_PERIOD;
+  }
+
+  if (lo_active) {
+    lo_active = 0;
+    last_event = 0;
+    do_work();
+  } else {
+    last_event++;
+  }
   //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_ON, TWI_OFF); // Turn CPU and selected subsystems off, Draws 1.01 mA
 }
 
